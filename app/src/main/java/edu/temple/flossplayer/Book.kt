cc2b3a489bd44e2c44f6c29ce9ecb37d73143968
NabaaAbdulhussein <111922494+NabaaAbdulhussein@users.noreply.github.com>
2022-11-02 22:18:16 -0400

    final

--- app/src/main/java/edu/temple/flossplayer/Book.kt
+++ app/src/main/java/edu/temple/flossplayer/Book.kt
@@ -2,11 +2,5 @@ package edu.temple.flossplayer
 
 import android.widget.TextView
 
-open class Book(var author: String, var title: String)
-{
-    lateinit var bookAuthor:TextView
-    lateinit var bookTitle: TextView
-
-
-    }
+open class Book(var author: String, var title: String){}
 
